dc07144de8e5c6ad985e67bb6efd42c6abb637c8
KaterynaS <zemlyanikak@gmail.com>
2020-01-13 11:11:55 -0800

    step counter back button abstract class toolbar help slider

--- app/src/main/java/com/example/castomtoolbar/ToolbarActivity.java
+++ app/src/main/java/com/example/castomtoolbar/ToolbarActivity.java
@@ -2,12 +2,24 @@ package com.example.castomtoolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class ToolbarActivity extends AppCompatActivity {
 
@@ -32,14 +44,47 @@ public abstract class ToolbarActivity extends AppCompatActivity {
             if(appState.isSoundOn()) { soundMenuItem.setIcon(R.drawable.ic_sound_on); }
             else { soundMenuItem.setIcon(R.drawable.ic_sound_off); }
         }
-
         return true;
     }
 
     public void openHelpPage()
     {
-        //todo open help slider
-        
+        //todo open help slider https://www.javatpoint.com/android-introduction-slider-example
+        Intent welcome = new Intent(getApplicationContext(), WelcomeActivity.class);
+        startActivity(welcome);
+
+
+
+        //popup window
+        // inflate the layout of the popup window
+//        LayoutInflater inflater = (LayoutInflater)
+//                getSystemService(LAYOUT_INFLATER_SERVICE);
+//        View popupView = inflater.inflate(R.layout.popup_window, null);
+//
+//        // create the popup window
+//        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+//        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+//        boolean focusable = true; // lets taps outside the popup also dismiss it
+//        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+//
+//        // show the popup window
+//        // which view you pass in doesn't matter, it is only used for the window tolken
+//        View view = findViewById(R.id.bar_of_tools);
+//        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+//
+//
+//        // dismiss the popup window when touched
+//        popupView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                popupWindow.dismiss();
+//                return true;
+//            }
+//        });
+    }
+
+    public void onButtonShowPopupWindowClick(View view) {
+
     }
 
     public abstract int getMenuResourceID();
